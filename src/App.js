a2bc7273ed9b1a8e5cc590cf0c151e51ec5bb4e7
MccGrey <chuxzzie01@yahoo.com>
2022-10-13 09:10:52 +0100

    update - add a STATE using USESTATE hook and assiged a default empty string to the variable, fname

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fname, setFname] = useState("");
   return (
     <div>
       <h1>React Form Handling</h1>
       <form>
         <label>
-          First Name: <input type="text" />
+          First Name: <input type="text" value={fname} />
         </label>
       </form>
+      <h5>First name: {fname}</h5>
     </div>
   );
 }

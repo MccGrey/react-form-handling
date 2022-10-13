8f3757dc7d2ee91185e768abcac484cf38ed4fa8
MccGrey <chuxzzie01@yahoo.com>
2022-10-13 09:18:36 +0100

    update - add an onChange handler that trigers and calls its handleChange function that re-render the state using setFname function

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React, { useState } from "react";
 
 function App() {
   const [fname, setFname] = useState("");
+
+  const handleChange = (e) => {
+    setFname(e.target.value);
+  };
   return (
     <div>
       <h1>React Form Handling</h1>
       <form>
         <label>
-          First Name: <input type="text" value={fname} />
+          First Name:{" "}
+          <input type="text" value={fname} onChange={handleChange} />
         </label>
       </form>
       <h5>First name: {fname}</h5>
